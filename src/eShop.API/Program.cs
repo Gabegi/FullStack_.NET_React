9b07d6808616e82fde5a8e5fbf1a79f2e837142b
Gabegi <gabriel_pirastru@hotmail.fr>
2025-04-23 10:13:01 +0400

    entity

--- src/eShop.API/Program.cs
+++ src/eShop.API/Program.cs
@@ -1,8 +1,15 @@
+using eShop.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("StoreConnectionString"));
+});
 
 
 var app = builder.Build();
