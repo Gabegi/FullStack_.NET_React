af7dbc6aeaf00bae40b70724305692caca7d27df
Gabegi <gabriel_pirastru@hotmail.fr>
2025-04-23 10:05:54 +0400

    entity

---
+++ src/eShop.API/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using eShop.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.API.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
