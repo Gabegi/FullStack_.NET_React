588900fc44d9d3e59594f814d3e27dd1328fd892
Gabegi <gabriel_pirastru@hotmail.fr>
2025-04-23 09:28:05 +0400

    entity

--- src/Product.cs
+++ src/Product.cs
@@ -4,9 +4,9 @@ public class Product
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
-    public long Price { get; set; }
-    public string PictureUrl { get; set; }
-    public string Type { get; set; }
-    public string Brand { get; set; }
+    public required long Price { get; set; }
+    public required string PictureUrl { get; set; }
+    public required string Type { get; set; }
+    public required string Brand { get; set; }
     public int QuantityInStock { get; set; }
 }
