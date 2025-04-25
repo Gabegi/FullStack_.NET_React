e60f7382c435b16496daa119358741a4f631fe05
Gabegi <gabriel_pirastru@hotmail.fr>
2025-04-25 22:37:07 +0400

    controller

---
+++ src/eShop.API/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿
+using eShop.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext context) : ControllerBase
+    {
+        [HttpGet] 
+        public 
+    }
+}
