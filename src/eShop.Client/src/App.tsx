1ef1f09b6608def2e1b872b673ba6eeef6bc85e6
Gabegi <gabriel_pirastru@hotmail.fr>
2025-04-29 20:03:37 +0400

    front page

--- src/eShop.Client/src/App.tsx
+++ src/eShop.Client/src/App.tsx
@@ -1,10 +1,16 @@
-
+const products = [
+  {name: 'product1', price:100.00},
+  {name: 'product2', price:200.00}
+]
 
 function App() {
 
   return (
       <div> 
- <h1>Restore</h1>        
+ <h1>Restore</h1>      
+ <ul>{products.map(item => (
+  <li>{item.name} - {item.price} </li>
+ ))}</ul>  
  </div>
 
   )
