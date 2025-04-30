74bba353d0b08a15f0f33b3121ae39c30d50b9f3
Gabegi <gabriel_pirastru@hotmail.fr>
2025-04-30 22:33:10 +0400

    front page

--- src/eShop.Client/src/App.tsx
+++ src/eShop.Client/src/App.tsx
@@ -1,15 +1,18 @@
-const products = [
-  { name: "product1", price: 100.0 },
-  { name: "product2", price: 200.0 },
-];
+import { useState } from "react";
+import { Fragment } from "react/jsx-runtime";
 
 function App() {
+  const [products, setProducts] = useState([
+    { name: "product1", price: 100.0 },
+    { name: "product2", price: 200.0 },
+  ]);
+
   return (
     <div>
       <h1>Restore</h1>
       <ul>
-        {products.map((item) => (
-          <li key={item.name}>
+        {products.map((item, index) => (
+          <li key={index}>
             {item.name} - {item.price}
           </li>
         ))}
