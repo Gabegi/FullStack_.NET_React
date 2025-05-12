7d55582d529f9ea59cace8bcb141b13afc47fa49
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-12 18:48:36 +0400

    adding productlist

--- src/eShop.Client/src/features/catalog/ProductCard.tsx
+++ src/eShop.Client/src/features/catalog/ProductCard.tsx
@@ -1,3 +1,17 @@
-export default function ProductCard() {
-  return <div>ProductCard</div>;
+import { Card, CardContent, CardMedia } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+type Props = { product: Product };
+
+export default function ProductCard({ product }: Props) {
+  return (
+    <Card elevation={3}>
+      <CardMedia
+        sx={{ height: 240, backgroundSize: "cover" }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+      <CardContent></CardContent>
+    </Card>
+  );
 }
