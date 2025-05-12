e865d6db86dbe6bd066bcbdfd2eb9a6a1a2c936b
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-12 18:28:15 +0400

    adding productlist

--- src/eShop.Client/src/features/catalog/Catalog.tsx
+++ src/eShop.Client/src/features/catalog/Catalog.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@mui/material";
 import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
 
 type Props = { products: Product[]; addProduct: () => void };
 
 export default function Catalog({ products, addProduct }: Props) {
   return (
     <div>
+      <ProductList products={products} />
       <Button variant="contained" onClick={addProduct}>
         Add Product by clicking
       </Button>
