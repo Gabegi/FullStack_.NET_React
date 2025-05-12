5d80481896cb69a413771fb98810467aaf8a41cf
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-12 18:25:25 +0400

    adding productlist

--- src/eShop.Client/src/features/catalog/Catalog.tsx
+++ src/eShop.Client/src/features/catalog/Catalog.tsx
@@ -6,13 +6,6 @@ type Props = { products: Product[]; addProduct: () => void };
 export default function Catalog({ products, addProduct }: Props) {
   return (
     <div>
-      <ul>
-        {products.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.price}
-          </li>
-        ))}
-      </ul>
       <Button variant="contained" onClick={addProduct}>
         Add Product by clicking
       </Button>
---
+++ src/eShop.Client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,17 @@
+import { Product } from "../../app/models/product";
+
+type Props = {
+  products: Product[];
+};
+
+export default function ProductList({ products }: Props) {
+  return (
+    <ul>
+      {products.map((item) => (
+        <li key={item.id}>
+          {item.name} - {item.price}
+        </li>
+      ))}
+    </ul>
+  );
+}
