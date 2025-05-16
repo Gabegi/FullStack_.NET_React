d8d015889be980b459b78c5b1d0a9136c61f5a29
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-16 08:40:58 +0400

    darkmode

--- src/eShop.Client/src/app/layout/Navbar.tsx
+++ src/eShop.Client/src/app/layout/Navbar.tsx
@@ -1,10 +1,13 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { DarkMode } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 
 export default function Navbar() {
+  const darkMode = true
   return (
     <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6">EShop</Typography>
+        <IconButton>{darkMode ? <DarkMode/>} </IconButton>
       </Toolbar>
     </AppBar>
   );
