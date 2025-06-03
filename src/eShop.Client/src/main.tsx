c015a9302b93982e2888f3ae90f5cd9e16f8d26a
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-03 11:19:28 +0500

    routing

--- src/eShop.Client/src/main.tsx
+++ src/eShop.Client/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app/layout/styles.css";
 import App from "./app/layout/App.tsx";
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
