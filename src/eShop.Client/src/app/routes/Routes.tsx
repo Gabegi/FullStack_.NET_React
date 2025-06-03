8c521fe3188767b40af6ea2634aeca1a6f3eb63c
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-03 11:08:29 +0500

    routing

--- src/eShop.Client/src/app/routes/Routes.tsx
+++ src/eShop.Client/src/app/routes/Routes.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import HomePage from "../../features/Home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 export const router = createBrowserRouter([
   {
@@ -8,10 +11,10 @@ export const router = createBrowserRouter([
     element: <App />, // components
     children: [
       { path: "", element: <HomePage /> },
-      { path: "", element: <HomePage /> },
-      { path: "", element: <HomePage /> },
-      { path: "", element: <HomePage /> },
-      { path: "", element: <HomePage /> },
+      { path: "/catalog", element: <Catalog /> },
+      { path: "/catalog/:id", element: <ProductDetails /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactPage /> },
     ],
   },
 ]);
