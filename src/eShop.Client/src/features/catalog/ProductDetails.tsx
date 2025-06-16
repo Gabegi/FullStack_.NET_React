df9859ec9bcdddfe8c681937d622547fb0551c51
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-16 08:28:18 +0500

    detail links

--- src/eShop.Client/src/features/catalog/ProductDetails.tsx
+++ src/eShop.Client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
   const { id } = useParams;
