fb01998215174d9177cf77e3c7e3b31778372aa4
Gabegi <gabriel_pirastru@hotmail.fr>
2025-06-16 08:27:40 +0500

    detail links

--- src/eShop.Client/src/features/catalog/ProductDetails.tsx
+++ src/eShop.Client/src/features/catalog/ProductDetails.tsx
@@ -1,3 +1,14 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function ProductDetails() {
-  return <div>ProductDetails</div>;
+  const { id } = useParams;
+  const [product, setProduct] = useState<Product | null>();
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products/${id}")
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, [id]);
+  return <div>{product?.name}</div>;
 }
